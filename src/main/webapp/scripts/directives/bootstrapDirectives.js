df6760c5fd6f3ce53e59bcf4168f31ef5aa528d6
Viorel Tacliciu <viorel.tacliciu@teamnet.ro>
2015-03-16 00:21:05 +0200

    resolved security/menu bugs

--- src/main/webapp/scripts/directives/bootstrapDirectives.js
+++ src/main/webapp/scripts/directives/bootstrapDirectives.js
@@ -1,1 +1,19 @@
 var bootstrapDirectives=angular.module('bootstrapDirectives',[]);
+bootstrapDirectives.directive('includeStatic', function($http, $templateCache, $compile) {
+    return {
+        restrict: 'A',
+        transclude: true,
+        replace: true,
+        scope:false,
+        link: function($scope, element, attrs, ctrl, transclude) {
+            var templatePath = attrs.includeStatic;
+
+
+            $http.get(templatePath, { cache: $templateCache })
+                .success(function(response) {
+                    var contents = element.html(response).contents();
+                    $compile(contents)($scope);
+                });
+        }
+    };
+});
