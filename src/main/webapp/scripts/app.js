583e8d354bf4d4661013d9a48b8fe91c2a5e4ec6
Viorel Tacliciu <viorel.tacliciu@teamnet.ro>
2015-04-09 18:57:05 +0300

    isolated security module

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -225,50 +225,58 @@ bootstrapApp
     })
     .run(function ($rootScope, $location, $http, AuthenticationSharedService, Session, AUTH_BOOTSTRAP) {
         $rootScope.authenticated = false;
+        if($rootScope.securityEnabled==undefined){
+            $rootScope.securityEnabled=true;
+        }
 
-        $rootScope.$on('$routeChangeStart', function (event, next) {
-            $rootScope.isAuthorized = AuthenticationSharedService.isAuthorized;
-            $rootScope.userModules = AUTH_BOOTSTRAP;
-            AuthenticationSharedService.valid(next.access.authorizedModules);
-        });
 
-        // Call when the the client is confirmed
-        $rootScope.$on('event:auth-loginConfirmed', function (data) {
-            $rootScope.authenticated = true;
-            if ($location.path() === "/login") {
-                var search = $location.search();
-                if (search.redirect !== undefined) {
-                    $location.path(search.redirect).search('redirect', null).replace();
-                } else {
-                    $location.path('/').replace();
+        if($rootScope.securityEnabled){
+            $rootScope.$on('$routeChangeStart', function (event, next) {
+                $rootScope.isAuthorized = AuthenticationSharedService.isAuthorized;
+                $rootScope.userModules = AUTH_BOOTSTRAP;
+                AuthenticationSharedService.valid(next.access.authorizedModules);
+            });
+
+            // Call when the the client is confirmed
+            $rootScope.$on('event:auth-loginConfirmed', function (data) {
+                $rootScope.authenticated = true;
+                if ($location.path() === "/login") {
+                    var search = $location.search();
+                    if (search.redirect !== undefined) {
+                        $location.path(search.redirect).search('redirect', null).replace();
+                    } else {
+                        $location.path('/').replace();
+                    }
                 }
-            }
-        });
+            });
 
-        // Call when the 401 response is returned by the server
-        $rootScope.$on('event:auth-loginRequired', function (rejection) {
-            Session.invalidate();
-            $rootScope.authenticated = false;
-            if ($location.path() !== "/" && $location.path() !== "" && $location.path() !== "/register" &&
-                $location.path() !== "/activate" && $location.path() !== "/login") {
-                var redirect = $location.path();
-                $location.path('/login').search('redirect', redirect).replace();
-            }
-        });
+            // Call when the 401 response is returned by the server
+            $rootScope.$on('event:auth-loginRequired', function (rejection) {
+                Session.invalidate();
+                $rootScope.authenticated = false;
+                if ($location.path() !== "/" && $location.path() !== "" && $location.path() !== "/register" &&
+                    $location.path() !== "/activate" && $location.path() !== "/login") {
+                    var redirect = $location.path();
+                    $location.path('/login').search('redirect', redirect).replace();
+                }
+            });
 
-        // Call when the 403 response is returned by the server
-        $rootScope.$on('event:auth-notAuthorized', function (rejection) {
-            $rootScope.errorMessage = 'errors.403';
-            $location.path('/error').replace();
-        });
+            // Call when the 403 response is returned by the server
+            $rootScope.$on('event:auth-notAuthorized', function (rejection) {
+                $rootScope.errorMessage = 'errors.403';
+                $location.path('/error').replace();
+            });
 
-        // Call when the user logs out
-        $rootScope.$on('event:auth-loginCancelled', function () {
-            $location.path('');
-        });
+            // Call when the user logs out
+            $rootScope.$on('event:auth-loginCancelled', function () {
+                $location.path('');
+            });
 
-        console.log(angular.element('.auth-data-required'));
-        angular.element('.auth-data-required').removeClass("auth-data-required");
+            console.log(angular.element('.auth-data-required'));
+            angular.element('.auth-data-required').removeClass("auth-data-required");
+        }
+
+
     })
     .run(function ($rootScope, $route) {
         // This uses the Atmoshpere framework to do a Websocket connection with the server, in order to send
