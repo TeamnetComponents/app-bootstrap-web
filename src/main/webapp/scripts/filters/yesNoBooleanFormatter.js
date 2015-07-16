862658b8c789e219fcead9328074b88526a14374
Oana.Mihai <oana.mihai@teamnet.ro>
2015-07-16 16:20:25 +0300

    updated formatters

--- src/main/webapp/scripts/filters/yesNoBooleanFormatter.js
+++ src/main/webapp/scripts/filters/yesNoBooleanFormatter.js
@@ -6,14 +6,17 @@ bootstrapFilters
             false: "No"
         };
         return function (someBooleanValue) {
-            if (typeof(someBooleanValue) === 'boolean')
-                return boolLabels[someBooleanValue];
-            if (typeof(someBooleanValue) === 'string')
-                return boolLabels[someBooleanValue.toLowerCase() === 'true' || someBooleanValue === '1'];
-            if (typeof(someBooleanValue) === 'number')
-                return boolLabels[someBooleanValue === 1];
             if (someBooleanValue === undefined || someBooleanValue === null)
                 return "N/A";
+            if (typeof(someBooleanValue) === 'boolean') {
+                return boolLabels[someBooleanValue];
+            }
+            if (typeof(someBooleanValue) === 'string') {
+                return boolLabels[someBooleanValue.toLowerCase() === 'true' || someBooleanValue === '1'];
+            }
+            if (typeof(someBooleanValue) === 'number') {
+                return boolLabels[someBooleanValue === 1];
+            }
             return someBooleanValue;
         }
     });
