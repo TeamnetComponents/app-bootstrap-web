534f1c1d954b273ccc2748a49fad9066afd0bd05
ionut.patrascu <ionut.patrascu@enea.com>
2015-07-17 17:32:43 +0300

    Adaugare ui-select2 pentru app-menu-admin.

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -10,7 +10,7 @@ var bootstrapApp = angular.module('bootstrapApp', ['http-auth-interceptor', 'tmh
     'ngResource', 'ngRoute', 'ngCookies', 'bootstrapAppUtils', 'pascalprecht.translate',
     'truncate', 'ngCacheBuster','bootstrapControllers','bootstrapServices','bootstrapDirectives','bootstrapConstants',
     'angular-component.app-grid', 'angular-components.app-menu', 'angular-components.app-menu-admin', 'ui.tree', 'ngMaterial',
-    'ngDragDrop', 'ui.select', 'ngSanitize']);
+    'ngDragDrop', 'ui.select','ui.select2' ,'ngSanitize']);
 
 angular.element(document).ready(function () {
     angularCustomLoader.loadApp(bootstrapApp);
