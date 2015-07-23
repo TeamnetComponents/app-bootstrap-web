0b7bee50742eec67645de85779e5047af3e97553
Cristian D <criss.dumitru.87@gmail.com>
2015-07-23 17:15:56 +0300

    Add N/A to date time formatter.

--- src/main/webapp/scripts/filters/simpleDateTimeFormatter.js
+++ src/main/webapp/scripts/filters/simpleDateTimeFormatter.js
@@ -2,6 +2,8 @@ bootstrapFilters
     .filter('simpleDateTimeFormatter',
     function () {
         return function (someDateTime) {
+            if (someDateTime === undefined || someDateTime === null)
+                return "N/A";
             return moment(someDateTime).format("DD-MM-YYYY HH:mm");
         }
     });
