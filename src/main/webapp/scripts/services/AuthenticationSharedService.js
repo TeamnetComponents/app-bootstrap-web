915e15f5406733b0bd0e039e545fa33c52bbece2
Cristian D <criss.dumitru.87@gmail.com>
2015-08-07 16:58:10 +0300

    Refresh page fix show route in nav.

--- src/main/webapp/scripts/services/AuthenticationSharedService.js
+++ src/main/webapp/scripts/services/AuthenticationSharedService.js
@@ -15,7 +15,6 @@ bootstrapServices.factory('AuthenticationSharedService',['$rootScope', '$http', 
                     $rootScope.account = Session;
                     Permissions.refreshAdminModules();
                     authService.loginConfirmed(data);
-                    $rootScope.sendInformationAboutAccount = data;
                 });
 
 
@@ -37,7 +36,7 @@ bootstrapServices.factory('AuthenticationSharedService',['$rootScope', '$http', 
                                 $rootScope.$broadcast("event:auth-notAuthorized");
                             } else {
                                 $rootScope.$broadcast("event:auth-loginConfirmed");
-                                $rootScope.$broadcast("event:getAccountInformation", $rootScope.sendInformationAboutAccount);
+                                $rootScope.$broadcast("event:getAccountInformation", data);
                             }
                         });
                     } else {
