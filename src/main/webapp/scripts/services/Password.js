44896eb26fb74affa80504e1fbb765ad020e1b49
vlad.licaret <vlad.licaret@teamnet.ro>
2016-01-26 18:21:05 +0200

    fix change password url

--- src/main/webapp/scripts/services/Password.js
+++ src/main/webapp/scripts/services/Password.js
@@ -1,5 +1,5 @@
 
 bootstrapServices.factory('Password',['$resource', function ($resource) {
-    return $resource('app/rest/account/change_password', {}, {
+    return $resource('app/rest/account/changePassword', {}, {
     });
 }]);
