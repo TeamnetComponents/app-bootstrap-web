d5d8bd4e57347de9feb1c3b315cebc901c310ef3
vlad.licaret <vlad.licaret@teamnet.ro>
2016-04-28 20:14:42 +0300

    audits dates fix

--- src/main/webapp/scripts/controllers/AuditsController.js
+++ src/main/webapp/scripts/controllers/AuditsController.js
@@ -2,6 +2,8 @@
 bootstrapControllers.controller('AuditsController',['$scope', '$translate', '$filter', 'AuditsService',
     function ($scope, $translate, $filter, AuditsService) {
     $scope.onChangeDate = function () {
+        if ($scope.fromDateObj) $scope.fromDate = $filter('date')($scope.fromDateObj, "yyyy-MM-dd");
+        if ($scope.toDateObj) $scope.toDate = $filter('date')($scope.toDateObj, "yyyy-MM-dd");
         AuditsService.findByDates($scope.fromDate, $scope.toDate).then(function (data) {
             $scope.audits = data;
         });
